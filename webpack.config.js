8c9c5a272911b0817f27d29730b6a55a54eb2118
campallison <allisonhanna@gmail.com>
2016-07-22 10:58:35 -0700

    adding packagejson file, webpack config file, appjs, and gitignore

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/, loader: 'style!css',
+      },
+    ],
+  },
+};
