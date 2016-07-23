a53f16209f8c79d2f28144ebf38c2f76c60cbf70
campallison <allisonhanna@gmail.com>
2016-07-23 14:50:29 -0700

    added favicon and loaders object

---
+++ client/loaders.js
@@ -0,0 +1,32 @@
+const loaders = {
+  json: {
+    loader: 'json-loader',
+    npm: 'npm install --save json-loader',
+    regEx: '/\.json$/',
+  },
+  css: {
+    loader: 'css-loader',
+    npm: 'npm install css-loader --save-dev',
+    regEx: '/\.css$/',
+  },
+  sass: {
+    loader: 'sass-loader',
+    npm: 'npm install sass-loader',
+    regEx: '/\.scss$/',
+  },
+  babel: {
+    loader: 'babel-loader',
+    npm: 'npm install babel-loader babel-core babel-preset-es2015 --save-dev',
+    regEx: '/\.js$/',
+  },
+  eslint: {
+    loader: 'eslint-loader',
+    npm: 'npm install eslint-loader',
+    regEx: '/\.js$/',
+  },
+  elm: {
+    loader: 'elm-webpack-loader',
+    npm: 'npm install --save elm-webpack-loader',
+    regEx: '/\.elm$/',
+  },
+};
